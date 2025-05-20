ba11e5efbb18786f75c2637127e68be6858cf9c2
dzhansu bashara <cansubasari@gmail.com>
2025-05-20 14:20:24 +0100

    cars mvc done

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,28 @@
+class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to @car, notice: 'Car was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :rental_option, :rent_price, :description, :features, :start_date, :end_date)
+  end
+end
