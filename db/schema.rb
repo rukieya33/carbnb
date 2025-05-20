16149b05afe9d06b2f8c74efb3a4b84cb100d7c0
Rukieya Ainoo <rukieya.a@gmail.com>
2025-05-20 15:03:05 +0100

    created controller bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_20_134551) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_20_134735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.string "rental_options"
+    t.float "rent_price"
+    t.text "description"
+    t.string "features"
+    t.string "start_date"
+    t.string "end_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_20_134551) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "cars", "users"
 end
