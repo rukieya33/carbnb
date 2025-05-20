2f8940c4c101f82a9976dabcd38df8d0a483b85d
Hamja Sultan <sultanhamja597@gmail.com>
2025-05-20 17:49:07 +0100

    designed navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'cars/index'
-  get 'cars/show'
-  get 'cars/new'
-  get 'cars/create'
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
