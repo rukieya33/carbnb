be59b9d85826c6e951a4be674a4b76b7520a7112
Rukieya Ainoo <rukieya.a@gmail.com>
2025-05-21 14:57:08 +0100

    car new updated cide

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 Car.destroy_all
 User.destroy_all
 
-user1 = User.create!(email:"fdhh@gmail.com", password: "12345", first_name: "Saddam",  last_name:"Hussain")
+user1 = User.create!(email:"fdhh@gmail.com", password: "sdfsdfs", first_name: "Saddam",  last_name:"Hussain")
 car1= Car.create!(brand: "ferrari", model:"3000", rental_options: "Daily", features:"Autopilot", start_date: "20/05/2025", end_date:"21/05/2025", user: user1 )
 
 puts "#{Car.count}"
