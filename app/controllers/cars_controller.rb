ce39d50ed491a192f66b21a95ae55f1f1bc2c6e2
Hamja Sultan <sultanhamja597@gmail.com>
2025-05-22 18:09:18 +0100

    show page design stat

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -9,6 +9,7 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
     @booking = Booking.new
+    @dates_booked = @car.bookings.map {|booking| [booking.start_date, booking.end_date] }
   end
 
   def new
