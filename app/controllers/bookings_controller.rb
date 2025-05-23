f4a0596585b6db8c104005c0ef186d0dfa252463
Saddam Hussain saddam5533@hotmail.com <saddam5533@hotmail.com>
2025-05-23 14:31:58 +0100

    bug fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -29,6 +29,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :photo)
   end
 end
